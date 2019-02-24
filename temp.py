7f605c75329c9d8e941a91e143fdc862b6738d58
209914097 <33018936+209914097@users.noreply.github.com>
2019-02-25 01:08:09 +0800

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,9 +1,7 @@
-class Solution:
-    def jumpFloor(self, number):
-        l=[0,1,2,3]
-        if number>3:
-            for j in range(4,(number+1)):
-                l.append(l[j-1]+l[j-2])
-        else:
-            return l[number]
-        return(l[number])
+class GoUpstairs:
+    defcountWays(self, n):
+        dp=[0,1,2]
+        if(n>2):
+            for x in range(3,n):
+                dp.append((dp[x-1]+dp[x-2])%1000000007)
+        return dp[n-1]
