caabe30a17ab1c77b02b81e1407d8e55e84e5082
209914097 <33018936+209914097@users.noreply.github.com>
2019-03-06 10:57:05 +0800

    心形Matlab

--- temp.py
+++ temp.py
@@ -1,22 +1,11 @@
-import sys
-https://www.nowcoder.com/questionTerminal/bf877f837467488692be703735db84e6
-s=sys.stdin.readlines()
-i=int(s[0].strip().split()[0])
-c=int(s[0].strip().split()[1])
-w=list(map(int,s[1].strip().split()))
-#w=[k  for k in w if k <= c]
-# i=3
-# c=10
-# w=[1,2,4]
-def f(w,c):
-    if len(w)==0:
-        return 1
-    elif w[0]<=c:
-        return f(w[1:],c)+f(w[1:],c-w[0])
-    else:
-        return f(w[1:],c)
-if (sum(w)<=c):
-    result=2**(len(w))
-else:
-    result=f(w,c)
-print(result)
+clear clc
+warning off
+for a=0:0.1:18
+    x=-1.8:0.001:1.8;
+    y=x.^(2/3)+0.9*abs((3.3-x.^2)).^(1/2).*sin(a*pi*x);
+    y(1:1800)=-y(1:1800);  
+    plot(x,y,'r');   
+    axis([-2,2,-2,2.5])
+    pause(0.05)
+end
+warning on
