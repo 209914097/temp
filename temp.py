315ce863fed998c115fa30b3f02ca4d970a1c5a7
209914097 <33018936+209914097@users.noreply.github.com>
2019-03-09 10:22:07 +0800

    1+2+3...+n

--- temp.py
+++ temp.py
@@ -1,11 +1,8 @@
-clear clc
-warning off
-for a=0:0.1:18
-    x=-1.8:0.001:1.8;
-    y=x.^(2/3)+0.9*abs((3.3-x.^2)).^(1/2).*sin(a*pi*x);
-    y(1:1800)=-y(1:1800);  
-    plot(x,y,'r');   
-    axis([-2,2,-2,2.5])
-    pause(0.05)
-end
-warning on
+import sys,math
+def f(x):
+    k=x
+    k=(x>0) and (k+f(x-1))
+    return k
+  
+ 
+print(f(5))
