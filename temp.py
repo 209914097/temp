ecd2725ef890fe166d0b604a393a36c03779e2b2
209914097 <33018936+209914097@users.noreply.github.com>
2019-08-11 10:58:31 +0000

    拼多多

--- temp.py
+++ temp.py
@@ -1,89 +1,65 @@
-# 题目:微信支付宝金额收款语音播报
-# 参考资料：https://tool.gaodun.com/rmb.html
-# 测试案例：
-# 0 零元
-# 0.0 零元
-# 0.00 零元
-# 1234567 壹佰贰拾叁万肆仟伍佰陆拾柒元
-# 1234567.01 壹佰贰拾叁万肆仟伍佰陆拾柒元壹分
-# 1234567.10 壹佰贰拾叁万肆仟伍佰陆拾柒元壹角
-# 10 拾元
-# 15 拾伍元
-# 21 贰拾壹元
-# 10.01 拾元壹分
-# 10.10 拾元壹角
-# 10.1 拾元壹角
-# 100010.10 拾万零拾元壹角
-# 100020.10 拾万零贰拾元壹角
-# 200010.10 贰拾万零拾元壹角
-# 0.1 壹角
-# 0.10 壹角
-# 0.01 壹分
-# 10001000 壹仟万零壹仟元
-a=200010.10
-intnum = str(a).split('.')[0]
-try:floatnum=str(a).split('.')[1]
-except:floatnum=''
+# 给定两个正数N和S找出所有的长度为N的正整数数列中，满足单调递增和总和为S的数列
+#输入：共一行，两个数N和S
+#输出：满足条件的所有队列
+#输入：3 10
+#输出：
+#[1, 2, 7]
+#[1, 3, 6]
+#[1, 4, 5]
+#[2, 3, 5]
+import sys
+# ss = input()
+# N =int( ss.split(' ')[0]);
+# S = int(ss.split(' ')[1]);
+N =3;
+S =10;
+#
+# print(S)
+# print(N)
+re=[]
+length =N
+def list_add(a,b):
+    c = []
+    for i in range(len(a)):
+        c.append(a[i]+b[i])
+    return c
+def ff(num,N,j):
+    result=[]
+    for i in range(N-num):
+        result.append(0)
+    for i in range(num):
+        result.append(j)
 
-l = len(intnum)
-intnum=list(intnum)
-ans=''
-money = ['零','壹','贰','叁','肆','伍','陆','柒','捌','玖']
-pos = ['','','拾','佰','仟','万','拾','佰','仟','亿']
-zero=False
-wan=True
-for i in intnum:
-    if i=='0':
-        zero=True
-    else:
-        if zero:
-            ans+='零'
-            zero = False
-        if pos[l]=='拾' and i=='1':  #对拾元单独处理，使之更符合日常读法，即10元读作拾元，不读作壹拾元
-            ans += pos[l]            #对拾元单独处理，使之更符合日常读法，即10元读作拾元，不读作壹拾元
+    return result
+
+def f(num,N,S,j):
+    global re
+    global base
+
+    if re==[]:
+        re=[base for i in range (N)]
+        num=num-1
+
+    if num>1:
+        re=list_add(re,ff(num,N,j))
+        if sum(re)<10:
+            num=num-1
+            f(num,N,S,j)
         else:
-            ans+=money[int(i)]
-            ans+=pos[l]
-        if pos[l]=='万':wan=False
-    l=l-1
-    if l==4 and wan==True: ans+='万'
-
-if ans=='' and (floatnum=='' or floatnum=='0'):ans='零元'
-elif ans=='' and floatnum!='':ans=''
-elif ans!='' and floatnum!='':ans+='元'
-elif ans!='' and floatnum=='':ans+='元'
-# 小数金额部分
-floatzero=False
-# if floatnum!='':
-#     if int(floatnum)!=0:
-#         ans += '点'
-#         for i in floatnum:
-#             if i=='0':
-#                 floatzero=True
-#             else:
-#                 if floatzero:
-#                     ans+='零'
-#                     floatzero = False
-#                 ans+=money[int(i)]
-
-if floatnum!='':
+            return
+    else:
+        err=(S-sum(re))
+        if err>=0:
+            j=j+1
+            re=list_add(re,ff(1,N,err))
+            print(re)
+            re=[]
+            f(N, N, S, j)
+        else:
+            return
 
 
-    if int(floatnum)!=0:
-        jiao=True
-        fen=False
-
-        floatl = len(floatnum)
-        for i in floatnum:
-            if i=='0':
-                jiao = False
-            else:
-                if jiao:
-                    ans += money[int(i)]
-                    ans+='角'
-                    jiao=False
-                else:
-                    ans+=money[int(i)]
-                    ans += '分'
-
-print(ans)
+for b in range(1,3):
+    base=b
+    re = []
+    f(3,3,12,1)
