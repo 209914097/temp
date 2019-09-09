dbc8094de7331ea5ea27bb9b11bdec854aa6b404
seel <382669441@qq.com>
2019-09-09 21:22:33 +0800

    奇安信

--- temp.py
+++ temp.py
@@ -1,65 +1,62 @@
-# 给定两个正数N和S找出所有的长度为N的正整数数列中，满足单调递增和总和为S的数列
-#输入：共一行，两个数N和S
-#输出：满足条件的所有队列
-#输入：3 10
-#输出：
-#[1, 2, 7]
-#[1, 3, 6]
-#[1, 4, 5]
-#[2, 3, 5]
-import sys
-# ss = input()
-# N =int( ss.split(' ')[0]);
-# S = int(ss.split(' ')[1]);
-N =3;
-S =10;
-#
-# print(S)
-# print(N)
-re=[]
-length =N
-def list_add(a,b):
-    c = []
-    for i in range(len(a)):
-        c.append(a[i]+b[i])
-    return c
-def ff(num,N,j):
-    result=[]
-    for i in range(N-num):
-        result.append(0)
-    for i in range(num):
-        result.append(j)
+# 编程题| 20分
+# 
+#       队尾幸运编号
+# 
+#       时间限制: C/C+ +语言1000MS ;其他语言3000MS内存限制: C/C++语言65536KB ;其他语言589824KB题目描述:
+# 
+#       N个人排成-队，从1到5轮流报数,报5的人是幸运者，出列。
+# 报到队尾后，从队首接着报。依此循环。问:排在队尾的人是第几名幸运者?注: N为小于100000的正整数。例:
+#       1人排成一队,他就是第1名幸运者。
+#       3人排成一一队，队尾是第2名幸运者。
+#       5人排成一队， 队尾是第1名幸运者。
+#       8人排成一一队，队尾是第3名幸运者
+#       即求: N人排成一队，队尾是第多少名幸运者?
+# 输入
+#       队伍总人数
+# 输出
+#       队尾者的幸运编号 
+# 样例输入
+#       20 
+# 样例输出
+#       4
 
-    return result
-
-def f(num,N,S,j):
-    global re
-    global base
-
-    if re==[]:
-        re=[base for i in range (N)]
-        num=num-1
-
-    if num>1:
-        re=list_add(re,ff(num,N,j))
-        if sum(re)<10:
-            num=num-1
-            f(num,N,S,j)
+def f(l,end):
+    ans=0
+    N = len(l)
+    while N>=5:
+        if l[4]==end:
+            ans += 1
+            return ans
         else:
-            return
-    else:
-        err=(S-sum(re))
-        if err>=0:
-            j=j+1
-            re=list_add(re,ff(1,N,err))
-            print(re)
-            re=[]
-            f(N, N, S, j)
-        else:
-            return
+            l.remove(l[4])
+            ans+=1
+            N=len(l)
+            one=l[4::]
+            two=l[0:4]
+            one.extend(two)
+            l=one
+    if (N<5):
+        mvindex=5%N-1
+        while l[mvindex] != end:
+            if(l[mvindex]==end):
+                ans += 1
+                return ans
+            else:
+                l.remove(l[mvindex])
+                ans += 1
+                one = l[mvindex::]
+                two = l[0:mvindex]
+                one.extend(two)
+                l = one
+                N = len(l)
+                mvindex = 5 % N - 1
+
+        ans += 1
+    return ans
+
+# ss=int(input())
+ss =8
+l=[i for i in range (1,ss+1)]
 
 
-for b in range(1,int(S/N)):
-    base=b
-    re = []
-    f(3,3,10,1)
+print(f(l,l[-1]))
