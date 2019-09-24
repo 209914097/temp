a7c0d5ae1cf07052c6290dad8a1ab48716d6545e
seel <382669441@qq.com>
2019-09-24 21:47:18 +0800

    百度笔试题

--- temp.py
+++ temp.py
@@ -1,62 +1,95 @@
-# 编程题| 20分
+#  题目描述:
 # 
-#       队尾幸运编号
+#       在N行M列的棋盘上,初始时棋子位于第x行第y列的方格上，且每个方格上都写有N、S. W. E四个字母中的一个。现在重复执行以下操作，直到棋子走出棋盘:
+#       每当棋子到达一个方格(包括初始时棋子所在的方格)时,
+#       ①若方格上写有字母N,则擦除方格上的字母,并将棋子向上移动格;②若方格上写有字母S ,则擦除方格上的字母，并将棋子向下移动格;③若方格上写有字母W，则擦除方格上的字母,并将棋子向左移动一格;④若方格上写有字母E ,则擦除方格上的字母, 并将棋子向右移动格;⑤若方格上的字母已被擦除，则将棋子沿着京先的方向再移动格。那么棋子总共移动了多少格才走出棋盘?
+# 输入:
+#       第一行输入两个整数N和M，1<=N，M<=2000。
+#       接下来N行,第i行输入一个长度为M且只包含字母N S. W. E的字符串,其中第j个字母即为棋盘第行第j列的方格上写的字母
+#       最后一行输入两个整数x和y，表示棋子的初始位置，1≤x≤N, 1≤y≤M。
 # 
-#       时间限制: C/C+ +语言1000MS ;其他语言3000MS内存限制: C/C++语言65536KB ;其他语言589824KB题目描述:
-# 
-#       N个人排成-队，从1到5轮流报数,报5的人是幸运者，出列。
-# 报到队尾后，从队首接着报。依此循环。问:排在队尾的人是第几名幸运者?注: N为小于100000的正整数。例:
-#       1人排成一队,他就是第1名幸运者。
-#       3人排成一一队，队尾是第2名幸运者。
-#       5人排成一队， 队尾是第1名幸运者。
-#       8人排成一一队，队尾是第3名幸运者
-#       即求: N人排成一队，队尾是第多少名幸运者?
-# 输入
-#       队伍总人数
 # 输出
-#       队尾者的幸运编号 
+#       输出棋子移动的格子数。
+# 
 # 样例输入
-#       20 
-# 样例输出
-#       4
+# 
+#       2 2
+#       SW
+#       EN
+#       2 1
+# 样例输出 5
+#       样例解释
+# 
+#       棋子先向右移动1格，再向上移动1格,接着向左移动1格,最后向下移动2格,总共移动了5格。
 
-def f(l,end):
-    ans=0
-    N = len(l)
-    while N>=5:
-        if l[4]==end:
-            ans += 1
-            return ans
-        else:
-            l.remove(l[4])
-            ans+=1
-            N=len(l)
-            one=l[4::]
-            two=l[0:4]
-            one.extend(two)
-            l=one
-    if (N<5):
-        mvindex=5%N-1
-        while l[mvindex] != end:
-            if(l[mvindex]==end):
-                ans += 1
-                return ans
-            else:
-                l.remove(l[mvindex])
-                ans += 1
-                one = l[mvindex::]
-                two = l[0:mvindex]
-                one.extend(two)
-                l = one
-                N = len(l)
-                mvindex = 5 % N - 1
+# N,M=map(int,input().split(' '))
+# for i in range(N):
+#     l=input()
+# X,Y=map(int,input().split(' '))
 
-        ans += 1
-    return ans
+N,M=3,3
+mapp=[['E', 'E','S'],
+      ['N','S','S'],
+      ['N','W','S']]
+X,Y=2,2
+x=X-1
+y=Y-1
 
-# ss=int(input())
-ss =8
-l=[i for i in range (1,ss+1)]
+# currentx=X
+# currenty=Y
+tem=''
+step=0
+try:
+    while 0<(x+1)<(N+1) and 0<(x+1)<(M+1):
+        if mapp[x][y]== 'E':
+            mapp[x][y] = 'b'
+            y += 1
+            step += 1
+            tem='E'
 
 
-print(f(l,l[-1]))
+        elif mapp[x][y]== 'W':
+            mapp[x][y] = 'b'
+            y -= 1
+            step += 1
+            tem = 'W'
+
+        elif mapp[x][y]== 'S':
+            mapp[x][y] = 'b'
+            x += 1
+            step += 1
+            tem = 'S'
+
+        elif mapp[x][y]== 'N':
+            mapp[x][y] = 'b'
+            x -= 1
+            step += 1
+            tem = 'N'
+
+        elif mapp[x][y]== 'b':
+
+            if tem == 'E':
+                y += 1
+                step += 1
+                if mapp[x][y]!= 'b':tem =  mapp[x][y]
+
+
+
+            elif tem == 'W':
+                y -= 1
+                step += 1
+                if mapp[x][y] != 'b': tem = mapp[x][y]
+
+            elif tem == 'S':
+                x += 1
+                step += 1
+                if mapp[x][y] != 'b': tem = mapp[x][y]
+
+            elif tem == 'N':
+                x -= 1
+                step += 1
+                if mapp[x][y] != 'b': tem = mapp[x][y]
+
+    print(step)
+except:
+    print(step)
