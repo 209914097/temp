f21d11e6c6cf305097b5fcf1e207283650ac4711
seel <382669441@qq.com>
2019-09-26 23:13:43 +0800

    输出迷宫的路径

--- temp.py
+++ temp.py
@@ -1,84 +1,45 @@
-# https://mp.weixin.qq.com/s/K8jLMtnwAqKUIGpNpiNB6A
-# https://www.ucloud.cn/yun/43129.html
-# 走迷宫
+#输出迷宫的路径
+m=4
+n=4
+flag=[[False for i in range(n)]for j in range(m)]
+maze=[[False for i in range(n)] for j in range(m)]
+direction=[[1,0],[-1,0],[0,1],[0,-1]]
 
+maze[1][1]=True#障碍物
+maze[1][2]=True
+maze[1][0]=True
+maze[2][2]=True
 
+end=[2,1]
+start=[0,0]
+minstep=999
+path=[]
+l=[]
+def DFS(x,y,step):
+    global minstep
+    global path
+    global l
+    if x==end[0] and y==end[1]:
+        if step<minstep:
+            minstep=step
+            l.append(path.copy())
 
-maze=[
-           [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-           [ 1, 0 ,0 ,0, 0 ,0 ,0 ,0 ,0, 0 ,1],
-           [ 1, 0 ,0 ,0 ,0 ,0 ,0 ,0, 0 ,1 ,1],
-           [ 1, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,1],
-           [ 1, 1 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,1 ,1],
-           [ 1 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,1 ,0 ,1],
-           [ 1 ,1 ,0 ,0 ,1 ,1 ,1 ,0 ,0 ,0 ,1],
-           [ 1 ,0 ,0 ,0 ,1 ,0 ,0 ,0 ,0 ,1 ,1],
-           [ 1 ,0 ,1 ,0 ,0 ,0 ,1 ,1 ,0 ,0 ,1],
-           [ 1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1 ,1]
-]
+        return
+    for i in range(4):
+        nt_x, nt_y = x + direction[i][0], y + direction[i][1]
+        if nt_x<0 or nt_x>= m or nt_y<0 or nt_y>=n:
 
-def mark(maze,pos):
-    maze[pos[0]][pos[1]]=2
+            continue
 
-def passable(maze,pos):
-    return maze[pos[0]][pos[1]]==0
+        if (flag[nt_x][nt_y] is False) and(maze[nt_x][nt_y]is False):
+            flag[nt_x][nt_y]=True
+            path.append([nt_x,nt_y])
+            DFS(nt_x,nt_y,step+1)
 
-dire=[(0,1),(1,0),(0,-1),(-1,0)]
-def st_find(maze, start, end):
-    """maze为迷宫矩阵，start和end是开始点和结束点
-    返回一个list"""
+            flag[nt_x][nt_y] = False
+            path.remove([nt_x,nt_y])
 
-    if start == end:
-        # 特殊情况，起点就是终点
-        return [(start, 0)]
-    st = []  # stack的缩写，表示栈
-    mark(maze, start)
-    st.append((start, 0))
-    # 将开始位置和要探索的未知未知压入栈
-    while len(st) != 0:
-        # 只要还有没有探索完的地方就不会结束
-        pos, nxt = st.pop()
-        for i in range(nxt, 4):
-            nextp = (pos[0] + dire[i][0], pos[1] + dire[i][1])
-            # 下一个探查点
-            if passable(maze, nextp):
-                st.append((pos, i + 1))
-                # 如果可行，把下一个没探测的点压入栈
-                mark(maze, nextp)
-                st.append((nextp, 0))
-                # 把新的位置等压入栈
-                if nextp == end:
-                    # 发现新位置是终点，结束
-                    return st
-                break
-    print("没有发现路径")
-    return []
-
-import matplotlib.pyplot as plt
-import matplotlib
-
-# 设置matplotlib正常显示中文和负号
-matplotlib.rcParams['font.sans-serif']=['SimHei']   # 用黑体显示中文
-matplotlib.rcParams['axes.unicode_minus']=False     # 正常显示负号
-
-
-
-st=st_find(maze,(1,1),(8,9))
-x=[]
-y=[]
-for i in st:
-    x.append(i[0][0])
-    y.append(i[0][1])
-
-plt.plot(x,y,'r')
-xq=[]
-yq=[]
-for i in range(10):
-    for j in range(11):
-        if maze[i][j]==1:
-            xq.append(i)
-            yq.append(j)
-
-plt.plot(xq,yq,'*')
-plt.title('深度优先搜索找到迷宫的路径')
-plt.show()
+DFS(3,3,0)
+# print(l) #输出所有到达出口的所有路径
+length=list(map(len,l))
+print(l[length.index(min(length))])#输出到达出口的最短路径
