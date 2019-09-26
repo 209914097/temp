7c01f271fa4926bdaeb6265d343517b3ce2d6a12
seel <382669441@qq.com>
2019-09-26 19:33:00 +0800

    全排列1,2,3,4

--- temp.py
+++ temp.py
@@ -1,62 +1,18 @@
-# 华为现场写代码
-# 广度优先遍历二叉树，找出key所在的层
-class node():
-    def __init__(self,value):
-        self.value=value
-        self.left=None
-        self.right=None
+# 全排列1,2,3,4
+# https://www.ucloud.cn/yun/43129.html
+N=4
+flag=[False for i in range (N)]
+st=[0 for  i in range(N)]
 
-n1=node(1)
-n2=node(2)
-n3=node(3)
-n4=node(4)
-n5=node(5)
-n6=node(6)
+def f (node):
+    if node ==3:
+        print(st)
+        return
+    for  i in range(N):
+        if flag[i] is False:
+            flag[i]=True
+            st[node]=i
+            f(node+1)
+            flag[i]=False
 
-n1.right=n2
-n1.left=n3
-n3.left=n4
-n3.right=n5
-n2.left=n6
-
-
-def BFS(node,key):
-    queue=[]
-    level = 0
-    queue.append(node)
-    level+=1
-    while queue:
-        for i in range(len(queue)):
-            currentnode=queue.pop(0)
-            if(currentnode.value==key):
-                print(level)
-
-            if currentnode.left:
-                queue.append(currentnode.left)
-            if currentnode.right:
-                queue.append(currentnode.right)
-
-        level+=1
-
-
-BFS(n1,6)
-
-
-def DFS(node):
-    stack=[]
-    level = 0
-    stack.append(node)
-    level+=1
-    while stack:
-        currentnode=stack.pop()
-        print(currentnode.value)
-
-        if currentnode.right:
-            stack.append(currentnode.right)
-        if currentnode.left:
-            stack.append(currentnode.left)
-DFS(n1)
-
-
-
-
+f(0)
